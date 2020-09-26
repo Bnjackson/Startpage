a6f64db13040a54173cff1f1cb57a44aac2c0e75
Bnjackson <65509144+Bnjackson@users.noreply.github.com>
2020-09-26 23:35:18 +0100

    Update script.js

---
+++ JS/script.js
@@ -0,0 +1,29 @@
+//Use dom manipulation to set time and change greeting based on time. 
+
+const timeHeading = document.getElementById("time-header");
+const greetingHeading = document.getElementById("greeting-header")
+
+
+setTimeGreeting();
+const interval = setInterval(setTimeGreeting, 1000);
+
+function setTimeGreeting() {
+    const time = new Date();
+    timeHeading.innerText = time.toLocaleTimeString("en-uk");
+    const hour = time.getHours();
+    if(hour >= 6 && hour < 12) {
+        greetingHeading.innerText = "Good Morning";
+    }
+    else if(hour >= 12 && hour < 18) {
+        greetingHeading.innerText = "Good Afternoon";
+    }
+    else if(hour >= 18 && hour < 24) {
+        greetingHeading.innerText = "Good Evening";
+    }
+    else {
+        greetingHeading.innerText = "Good Night";
+    }
+    console.log(hour);    
+}
+
+
